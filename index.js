16688659caa76f893205f5b0d79d0ccbacacb5af
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-28 19:24:56 +0300

    finished assignment

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
+   get the length of the string and divided into two
+   iterate from the beginning of the string  upto the middle
+   while iterating compare the first to the last letter
 */
+for (const startIndex = 0; strartIndex < word.length / 2, startIndex++){
+  const endIndex = word.length - 1 -startIndex
+  if(word[startIndex !== word[endIndex]]){ 
+    return false
+  }
+  return true
+}
 
 /*
   Add written explanation of your solution here
